a8cd9f552d2bbfa5bc3e9742e17e864663424ef8
zhuwenqi <zhuwenqi@zto.cn>
2019-07-22 20:41:04 +0800

    new

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack')
 // const NODE_ENV = process.env.NODE_ENV
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
